c0d8c8e32844acea38b654654a0d1749f1f320b0
eb_cmgonzalezo <eb_cmgonzalezo@bancoripley.com>
2023-02-08 16:40:58 -0300

    feat:controlador de usuarios creado

---
+++ controllers/users.controller.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+
+
+export const getUsers = ( req:Request, res:Response ) => {
+
+    res.json({
+        msg : 'getUsers'
+    })
+}
+
+export const getUser = ( req:Request, res:Response ) => {
+
+    const { id } = req.params
+
+    res.json({
+        msg : 'getUser',
+        id
+    })
+}
+
+export const postUser = ( req:Request, res:Response ) => {
+
+    const { body } = req
+
+    res.json({
+        msg : 'postUser',
+        body
+    })
+}
+
+export const putUser = ( req:Request, res:Response ) => {
+
+    const { id } = req.params
+    const { body } = req
+
+    res.json({
+        msg : 'putUser',
+        body,
+        id
+    })
+}
+
+export const deleteUser = ( req:Request, res:Response ) => {
+
+    const { id } = req.params
+
+    res.json({
+        msg : 'deleteUser',
+        id
+    })
+}
---
+++ dist/controllers/users.controller.js
@@ -0,0 +1,44 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteUser = exports.putUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
+const getUsers = (req, res) => {
+    res.json({
+        msg: 'getUsers'
+    });
+};
+exports.getUsers = getUsers;
+const getUser = (req, res) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'getUser',
+        id
+    });
+};
+exports.getUser = getUser;
+const postUser = (req, res) => {
+    const { body } = req;
+    res.json({
+        msg: 'postUser',
+        body
+    });
+};
+exports.postUser = postUser;
+const putUser = (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    res.json({
+        msg: 'putUser',
+        body,
+        id
+    });
+};
+exports.putUser = putUser;
+const deleteUser = (req, res) => {
+    const { id } = req.params;
+    res.json({
+        msg: 'deleteUser',
+        id
+    });
+};
+exports.deleteUser = deleteUser;
+//# sourceMappingURL=users.controller.js.map
